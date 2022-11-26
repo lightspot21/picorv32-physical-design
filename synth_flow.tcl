3a1f0efe68dc3da1d26005c72dafb1dfceebdc4d
Grigorios Pavlakis <grigpavl@ece.auth.gr>
2022-11-26 17:43:50 +0200

    Milestone: Rework script setup to adapt for layout flow
    Move all variables that are shared by flows to
    global_vars.tcl that all flows will import before starting.
    
    Convention: To set "local variable", prepend it with the flow's
    name.

--- synth_flow.tcl
+++ synth_flow.tcl
@@ -1,29 +1,25 @@
-# Set current directory root as variable
-set ROOT [file normalize [file dirname [info script]]]
-set LIB_ROOT /mnt/apps/prebuilt/eda/designkits/GPDK/gsclib045/lan/flow/t1u1/reference_libs/GPDK045/gsclib045_svt_v4.4/gsclib045/
+# Get global settings
+source ./global_vars.tcl
 
-set DATA $ROOT/data
-set SCRIPTS $ROOT/scripts
-set STAGES $SCRIPTS/stages
-set OUTPUT $ROOT/out
-set OUT_RPT $OUTPUT/reports
+# Set script+report directories
+set SYNTH_SCRIPTS $SCRIPTS/synth
+set SYNTH_REPORTS $OUTPUT/synth-reports
 
 # Intermediate files used between flows
-set OUTPUT_INTER $OUTPUT/intermediate
-set OUT_DSN $OUTPUT_INTER/design
-set OUT_GEN $OUTPUT_INTER/genus_to_innovus
+set SYNTH_INTERM $INTERMEDIATE/synth
+set SYNTH_INTERM_DSN $SYNTH_INTERM/design
 
 # Set paths for scripts, Verilog files and library root
 set_db init_lib_search_path $LIB_ROOT
 set_db init_hdl_search_path $DATA 
-set_db script_search_path   $SCRIPTS
+set_db script_search_path   $SYNTH_SCRIPTS
 
 # Load technology libraries
-source [ file join $STAGES init_libraries.tcl ]
+source [ file join $SYNTH_SCRIPTS init_libraries.tcl ]
 # Prepare HDL
-source [ file join $STAGES load_hdl.tcl ]
+source [ file join $SYNTH_SCRIPTS load_hdl.tcl ]
 # Declare timing constraints
-source [ file join $STAGES make_sdc.tcl]
+source [ file join $SYNTH_SCRIPTS make_sdc.tcl]
 
 # Check if timing constraints are complete
 check_timing_intent
@@ -37,16 +33,16 @@ syn_map
 syn_opt
 
 # Export reports
-report_area > $OUT_RPT/area.txt
-report_power > $OUT_RPT/power.txt
-report_timing > $OUT_RPT/timing.txt
-report_gates > $OUT_RPT/gates.txt
-report_qor > $OUT_RPT/qor.txt
+report_area > $SYNTH_REPORTS/area.txt
+report_power > $SYNTH_REPORTS/power.txt
+report_timing > $SYNTH_REPORTS/timing.txt
+report_gates > $SYNTH_REPORTS/gates.txt
+report_qor > $SYNTH_REPORTS/qor.txt
 
 # Export final design+constraints used
-write_hdl > $OUT_DSN/design.v
-write_sdc > $OUT_DSN/constraints.sdc
+write_hdl > $SYNTH_INTERM_DSN/design.v
+write_sdc > $SYNTH_INTERM_DSN/constraints.sdc
 
 # Export final design files for Innovus
 write_design -innovus picorv32
-file rename ./genus_invs_des $OUT_GEN
+file rename ./genus_invs_des $INTERM_GENUS_INV
