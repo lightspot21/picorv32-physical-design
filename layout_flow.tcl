a6e2d7b3c5463c7cd1ed60ad0c6c975be93f6d23
Grigorios Pavlakis <grigpavl@ece.auth.gr>
2022-12-29 17:12:46 +0200

    Correction: Top metal is Metal11, Metal1 is the bottom of the stack

--- layout_flow.tcl
+++ layout_flow.tcl
@@ -23,7 +23,7 @@ create_floorplan -site CoreSite -core_density_size 1 0.75 15 15 15 15
 # (width = spacing = 3 microns, centered in channel
 # between core and I/O)
 add_rings -nets {VDD VSS} -type core_rings -follow core \
- -layer {top Metal1 bottom Metal1 left Metal2 right Metal2} \
+ -layer {top Metal11 bottom Metal11 left Metal10 right Metal10} \
  -width {top 3 bottom 3 left 3 right 3} \
  -spacing {top 3 bottom 3 left 3 right 3} \
  -offset {top 1.8 bottom 1.8 left 1.8 right 1.8} \
@@ -31,7 +31,7 @@ add_rings -nets {VDD VSS} -type core_rings -follow core \
 
 # Create power stripes (VDD, VSS)
 # (3 pairs, same width and spacing as with rings)
-add_stripes -nets {VDD VSS} -layer Metal1 \
+add_stripes -nets {VDD VSS} -layer Metal10 \
  -direction vertical \
  -width 3 -spacing 3 -number_of_sets 3
 
@@ -42,6 +42,6 @@ connect_global_net VSS -type pg_pin -pin_base_name VSS -all
 connect_global_net VSS -type tie_lo -inst_base_name *
 
 # Create power+ground pins and connect with rings
-create_pg_pin -name VDD -net VDD -geom Metal1 0 9 12 18
-create_pg_pin -name VSS -net VSS -geom Metal1 0 201.8 8 218.8
-update_power_vias -add_vias 1 -top_layer Metal1 -bottom_layer Metal2 -area {10 16 11 17}
+create_pg_pin -name VDD -net VDD -geom Metal11 0 9 12 18
+create_pg_pin -name VSS -net VSS -geom Metal11 0 201.8 8 218.8
+update_power_vias -add_vias 1 -top_layer Metal11 -bottom_layer Metal10 -area {10 16 11 17}
