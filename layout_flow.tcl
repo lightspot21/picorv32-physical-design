ad434e577bbe30c9da285cdd2c83018fccee0468
Grigorios Pavlakis <grigpavl@ece.auth.gr>
2022-12-29 17:57:01 +0200

    Add power planning for follow pins

--- layout_flow.tcl
+++ layout_flow.tcl
@@ -46,3 +46,15 @@ create_pg_pin -name VDD -net VDD -geom Metal11 0 13 12 19
 create_pg_pin -name VSS -net VSS -geom Metal11 0 201 6 207
 update_power_vias -add_vias 1 -top_layer Metal11 -bottom_layer Metal10 -area {9 13 12 19}
 update_power_vias -add_vias 1 -top_layer Metal11 -bottom_layer Metal10 -area {3 201 6 207}
+
+# Create follow pins (logic-to-power connections)
+set_db route_special_via_connect_to_shape { stripe }
+route_special -connect core_pin \
+ -layer_change_range { Metal1(1) Metal11(11) } \
+ -block_pin_target nearest_target \
+ -core_pin_target first_after_row_end \
+ -allow_jogging 1 \
+ -crossover_via_layer_range { Metal1(1) Metal11(11) } \
+ -nets { VSS VDD } -allow_layer_change 1 \
+ -target_via_layer_range { Metal1(1) Metal11(11) }
+
